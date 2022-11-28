4f8d2331045d81102d865d76312a6023d6e8585d
rongrongya <119348923+rongrongya@users.noreply.github.com>
2022-11-29 03:21:24 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -24,7 +24,7 @@ oPUbI5grHDo05JsNaBSot5VR1Dmc
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '',config/template-config.cjs → 配置/template-config.cjs
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
