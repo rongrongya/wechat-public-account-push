ce9f1da26299fde8b0a78239dd7e6060c57e9d69
rongrongya <119348923+rongrongya@users.noreply.github.com>
2022-11-29 03:16:21 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -10,9 +10,9 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 wxb3d654b15fc115a8
+
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
-
 e664277108e7e6ac8fcad979ce131410
   PROVINCE: '广东',
   CITY: '惠州',
